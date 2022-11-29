94306344b58cd27a50e8dd3347c74cffc60dbb1b
Le Dinh Khoi <dinhkhoi.le3@gmail.com>
2022-11-29 16:23:22 +0700

    TSD-4: navigator

--- sources/public/assets/js/home/home.js
+++ sources/public/assets/js/home/home.js
@@ -5,3 +5,35 @@ $(document).ready(function(){
         margin: 40
     });
   });
+
+// $(document).ready(function () {
+//     $(".dropdown-submenu a.submenu").on("click", function (e) {
+//         console.log($(this))
+//       $(this).next("ul").toggle();
+//       e.preventDefault();
+//       e.stopPropagation();
+//       var index = e.target.dataset.index
+//     })
+//     $(".dropdown-submenu a.submenu").on('click', function closeMenu (e){
+//         console.log($(this))
+
+//         if($(".dropdown-submenu a.submenu").has(e.target).length === 0){
+//             $(this).next("ul").removeClass('show');
+//         } else {
+//             $(document).one('click', closeMenu);
+//         }
+//     });
+//   });
+
+$(document).ready(function() {
+    $(".toggle-submenu").on('click', function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        var dropdown = $(this).siblings('ul')
+        dropdown.toggle()
+        dropdown.css({
+                "position": "static",
+        });
+    })
+})
+
