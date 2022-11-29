d8dc9f092056cc7b17217e3a214528b21e4d94f8
Dự Nguyễn Chiếm <dunguyenchiem@Dus-MacBook-Pro.local>
2022-11-29 16:34:50 +0700

    feature: policy section and list product

--- sources/public/assets/js/home/home.js
+++ sources/public/assets/js/home/home.js
@@ -1,7 +1,63 @@
-$(document).ready(function(){
-    $(".owl-carousel").owlCarousel({
-        items : 4,
+$(document).ready(function () {
+    $(".product-category").owlCarousel({
+        items: 4,
         loop: true,
-        margin: 40
+        margin: 40,
+        responsive: {
+            1024: {
+                items: 4
+            },
+            800: {
+                items: 3,
+                autoplay: true
+
+            },
+            600: {
+                items: 2,
+                autoplay: true
+
+            },
+            100: {
+                items: 2,
+                autoplay: true
+
+            },
+        }
     });
-  });
+    $(".policy-row").owlCarousel({
+        items: 4,
+        loop: true,
+        margin: 30,
+        mouseDrag: false,
+        touchDrag: false,
+        autoHeight: false,
+        responsive: {
+            1024: {
+                items: 4,
+            },
+            800: {
+                items: 3,
+                mouseDrag: true,
+                touchDrag: true,
+                autoplay: true
+            },
+            600: {
+                items: 1,
+                mouseDrag: true,
+                touchDrag: true,
+                autoplay: true
+
+            },
+            100: {
+                items: 1,
+                mouseDrag: true,
+                touchDrag: true,
+                autoplay: true
+            },
+        }
+    });
+});
+
+
+
+
