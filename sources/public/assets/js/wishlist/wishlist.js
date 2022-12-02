9f0a9189cd8b5aeafbc6533eabebb4144319aaa5
Le Dinh Khoi <dinhkhoi.le3@gmail.com>
2022-12-02 12:58:07 +0700

    TSD-5: wishlist: fix bug UI

--- sources/public/assets/js/wishlist/wishlist.js
+++ sources/public/assets/js/wishlist/wishlist.js
@@ -1,11 +1,20 @@
+var width_default = window.innerWidth;
 
 var mobile = document.getElementById('wishlist-mobile');
 var web = document.getElementById('wishlist-web');
 
+
+if (width_default <1200) {
+    mobile.style.display = 'block';
+    web.style.display = 'none';
+} else {
+    mobile.style.display = 'none';
+    web.style.display = 'block';
+}
+
 window.onresize = function() {
-    let width = window.innerWidth;
-    console.log(width);
-    if (width <1200) {
+    let width_resize = window.innerWidth
+    if (width_resize <1200)     {
         mobile.style.display = 'block';
         web.style.display = 'none';
     } else {
