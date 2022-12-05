efb4bf95b80ac6b7390ed9d947482f701acf1710
Le Dinh Khoi <dinhkhoi.le3@gmail.com>
2022-12-05 22:49:09 +0700

    TSD-10: UI checkout

--- sources/controllers/home_controller.js
+++ sources/controllers/home_controller.js
@@ -27,6 +27,9 @@ class HomeController {
     async wishlist(req, res) {
         res.render("wishlist/wishlist", { user: req.headers.userInfor })
     }
+    async checkout(req, res) {
+        res.render("checkout/checkout", { user: req.headers.userInfor})
+    }
 }
 
 module.exports = new HomeController();
--- sources/routes/home.js
+++ sources/routes/home.js
@@ -2,6 +2,7 @@ var express = require("express");
 const home_controller = require("../controllers/home_controller");
 var router = express.Router()
 
+router.get('/checkout', home_controller.checkout)
 router.get('/cart', home_controller.cart)
 router.get("/wishlist", home_controller.wishlist)
 router.get("/", home_controller.home)
